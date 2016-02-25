cf9f47e88a5a2ae8dc5e7b75eff350a85d9cb163
Alejandro <eanotolic@yahoo.com>
2016-02-24 23:34:27 -0800

    more small changes and an asset precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # NOTE: manually changed to true to fix heroku
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
