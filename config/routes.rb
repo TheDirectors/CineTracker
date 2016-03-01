20cfd37d325f7820bb73938da4e5ffe00d7b0093
HolyPkerz <holypkerz@gmail.com>
2016-03-01 12:04:27 -0800

    Finish log in/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
+  
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   
   match '/signup', to: 'users#new', via: 'get'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217034020) do
+ActiveRecord::Schema.define(version: 20160227041557) do
 
   create_table "episodes", force: :cascade do |t|
     t.integer  "season"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20160217034020) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "salt"
   end
 
 end
