d2d6b25107ec4be632e6b3cab4b0122d6cb90a91
HolyPkerz <holypkerz@gmail.com>
2016-03-02 20:51:08 -0800

    login + logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
-		redirect_to root_path
+	  log_in @user
+	  redirect_to root_path
     else
       render 'new'
     end
