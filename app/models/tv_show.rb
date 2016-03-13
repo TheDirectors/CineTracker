1fa9d8ad3a837557b8192f288959a6c89e50fd67
Alejandro <eanotolic@yahoo.com>
2016-03-13 01:25:39 -0800

    added info page for search results

--- app/models/tv_show.rb
+++ app/models/tv_show.rb
@@ -13,23 +13,32 @@ class TvShow < ActiveRecord::Base
 					break
 				end
 				if mov["poster_path"] != nil
-					@results << @base_url + mov["poster_path"]
+					@results << [@base_url + mov["poster_path"], "tv=" + mov[:id].to_s]
 				end
 				@count += 1
 			end
 		else
 			@results = [
-				"deadpool-poster.jpg", 
-	            "interstellar-poster.jpg", 
-	            "mad-max-fury-road-poster.jpg", 
-	            "the-martian-poster.jpg", 
-	            "star-wars-the-force-awakens-poster.jpg", 
-	            "spectre-poster.jpg", 
-	            "kingsman-the-secret-service-poster.jpg", 
-	            "ex-machina-poster.jpg", 
-	            "sicario-poster.jpg", 
-	            "the-revenant-poster.jpg"]
+				["deadpool-poster.jpg", "mov=293660"], 
+	            ["interstellar-poster.jpg", "mov=157336"], 
+	            ["mad-max-fury-road-poster.jpg", "mov=76341"], 
+	            ["the-martian-poster.jpg", "mov=286217"], 
+	            ["star-wars-the-force-awakens-poster.jpg", "mov=140607"], 
+	            ["spectre-poster.jpg", "mov=206647"], 
+	            ["kingsman-the-secret-service-poster.jpg", "mov=207703"], 
+	            ["ex-machina-poster.jpg", "mov=264660"], 
+	            ["sicario-poster.jpg", "mov=273481"], 
+	            ["the-revenant-poster.jpg", "mov=281957"]]
 		end
 		return @results
+	end
+
+	def self.info(query)
+		Tmdb::Api.key(ENV["TMDB_KEY"])
+		@result = []
+		if query != nil && query != ""
+			@result = Tmdb::TV.detail(query)
+		end
+		return @result
 	end
 end
