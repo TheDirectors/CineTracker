2bb47775712d0c8d4a6f3b6779a28f48732ee024
Alejandro <eanotolic@yahoo.com>
2016-04-16 18:53:04 -0700

    added recommendations

--- app/models/tv_show.rb
+++ app/models/tv_show.rb
@@ -32,7 +32,36 @@ class TvShow < ActiveRecord::Base
 		end
 		return @results
 	end
-
+	def self.similar(query)
+		Tmdb::Api.key(ENV["TMDB_KEY"])
+		@base_url = Tmdb::Configuration.get.images.base_url + Tmdb::Configuration.get.images.poster_sizes[3]
+		@count = 0
+		@results = []
+		if query != nil && query != ""
+			Tmdb::TV.similar(query).results.each do |mov|
+				if @count >= 10
+					break
+				end
+				if mov["poster_path"] != nil
+					@results << [@base_url + mov["poster_path"], "mov=" + mov[:id].to_s]
+				end
+				@count += 1
+			end
+		else
+			@results = [
+				["deadpool-poster.jpg", "mov=293660"], 
+	            ["interstellar-poster.jpg", "mov=157336"], 
+	            ["mad-max-fury-road-poster.jpg", "mov=76341"], 
+	            ["the-martian-poster.jpg", "mov=286217"], 
+	            ["star-wars-the-force-awakens-poster.jpg", "mov=140607"], 
+	            ["spectre-poster.jpg", "mov=206647"], 
+	            ["kingsman-the-secret-service-poster.jpg", "mov=207703"], 
+	            ["ex-machina-poster.jpg", "mov=264660"], 
+	            ["sicario-poster.jpg", "mov=273481"], 
+	            ["the-revenant-poster.jpg", "mov=281957"]]
+		end
+		return @results
+	end
 	def self.info(query)
 		Tmdb::Api.key(ENV["TMDB_KEY"])
 		@result = []
